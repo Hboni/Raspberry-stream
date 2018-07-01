a0f10a0dd2d94ac93cf8d29fb8e7730c6504ab56
Hugo <hugoboniface@gmail.com>
2018-07-01 16:12:58 +0200

    Create a main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from server import Server
+from client import Client
+
+port = 15555
+choice = input("Server [s] or Client [c] ? ")
+
+if choice == 's':
+    serv = Server(port)
+    serv.begin_listen()
+elif choice == 'c':
+    client = Client(port)
+    client.send_message()
