5bf5dc901efdd96edc5a77f0485a9449c7714a29
Hugo <hugoboniface@gmail.com>
2018-07-02 19:41:24 +0200

    Create Client.chat_with_server function to send several messages to the server

--- client.py
+++ client.py
@@ -11,8 +11,8 @@ class Client:
         self.socket.connect((self.hote, self.port))
         print("Connection on {}".format(self.port))
 
-    def send_message(self):
-        self.socket.send("Hey my name is Hugo!".encode())
+    def send_message(self, message):
+        self.socket.send(message.encode())
 
         response = ""
         while not response:
@@ -21,6 +21,24 @@ class Client:
 
         print("Close")
         self.socket.close()
+
+
+    def chat_with_server(self):
+        mess = input("Which message to send to the server : \n")
+
+        server_response = ""
+        while mess != "fin":
+            self.socket.send(mess.encode())
+            #server_response = self.socket.recv(1024)
+            mess = input("Which message to send to the server : \n")
+
+        self.socket.send(mess.encode())
+
+        server_response = self.socket.recv(1024)
+        print(server_response.decode())
+        print("Close")
+        self.socket.close()
+
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ choice = input("Server [s] or Client [c] ? ")
 if choice == 's':
     serv = Server(port)
     serv.begin_listen()
+
 elif choice == 'c':
     client = Client(port)
-    client.send_message()
+    mess = input("Which message to send to the server ? :\n")
+    # client.send_message(mess)
+    client.chat_with_server()
