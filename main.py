df5718ecfc7ffbeb0dd7f0de14cf3bbde3029193
Hugo <hugoboniface@gmail.com>
2018-07-03 20:34:22 +0200

    Add port choice

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from server import Server
 from client import Client
 
-port = 15555
 choice = input("Server [s] or Client [c] ? ")
+port = input("Which port to use ? ")
+if not port:
+    port = 14142
 
 if choice == 's':
     serv = Server(port)
