95b6583bbb7b1c3e78f440fb1d2f85ace132d465
Hugo <hugoboniface@gmail.com>
2018-07-07 19:35:26 +0200

    Add groupboxes, a state message and an input lineedit

--- client_ui.py
+++ client_ui.py
@@ -5,6 +5,10 @@ class ClientUI(QtGui.QWidget):
     def __init__(self):
 
         super(ClientUI, self).__init__()
+        self.setupUI()
+        self.setupEvents()
+
+    def setupUI(self):
 
         self.main_layout = QtGui.QVBoxLayout(self)
 
@@ -16,32 +20,55 @@ class ClientUI(QtGui.QWidget):
         self.title_label.setFont(title_font)
         self.title_layout.addWidget(self.title_label)
 
+        self.state_box = QtGui.QGroupBox()
+        self.state_layout = QtGui.QHBoxLayout()
+        self.state_label = QtGui.QLabel("Not connected")
+        self.state_layout.addWidget(self.state_label)
+        self.state_box.setLayout(self.state_layout)
+        self.title_layout.addWidget(self.state_box)
+
         self.main_layout.addLayout(self.title_layout)
 
         # Parameters fillable content
-        self.param_layout = QtGui.QVBoxLayout()
+        self.param_box = QtGui.QGroupBox("Parameters")
+        self.param_layout = QtGui.QGridLayout()
 
         self.address_layout = QtGui.QHBoxLayout()
         self.address_label = QtGui.QLabel("Address : ")
         self.address_lineedit = QtGui.QLineEdit()
-        self.address_layout.addWidget(self.address_label)
-        self.address_layout.addWidget(self.address_lineedit)
+        self.param_layout.addWidget(self.address_label, 0, 0)
+        self.param_layout.addWidget(self.address_lineedit, 0, 1)
 
         self.port_layout = QtGui.QHBoxLayout()
         self.port_label = QtGui.QLabel("Port : ")
         self.port_lineedit = QtGui.QLineEdit()
-        self.port_layout.addWidget(self.port_label)
-        self.port_layout.addWidget(self.port_lineedit)
+        self.param_layout.addWidget(self.port_label, 1, 0)
+        self.param_layout.addWidget(self.port_lineedit, 1, 1)
 
-        self.param_layout.addLayout(self.address_layout)
-        self.param_layout.addLayout(self.port_layout)
+        self.param_box.setLayout(self.param_layout)
+        self.main_layout.addWidget(self.param_box)
 
-        self.main_layout.addLayout(self.param_layout)
+        self.input_box = QtGui.QGroupBox("Inputs")
+        self.input_layout = QtGui.QHBoxLayout()
+        self.input_link_label = QtGui.QLabel("Stream address : ")
+        self.input_layout.addWidget(self.input_link_label)
+        self.input_link_lineedit = QtGui.QLineEdit()
+        self.input_layout.addWidget(self.input_link_lineedit)
+        self.input_box.setLayout(self.input_layout)
+        self.main_layout.addWidget(self.input_box)
 
+        self.button_layout = QtGui.QHBoxLayout()
         self.connect_button = QtGui.QPushButton("Connect")
-        self.main_layout.addWidget(self.connect_button)
+        self.button_layout.addWidget(self.connect_button)
+        self.send_button = QtGui.QPushButton("Send")
+        self.button_layout.addWidget(self.send_button)
+        self.cancel_button = QtGui.QPushButton("Cancel")
+        self.button_layout.addWidget(self.cancel_button)
 
-        self.show()
+        self.main_layout.addLayout(self.button_layout)
+
+    def setupEvents(self):
+        self.cancel_button.clicked.connect(self.close)
 
 
 if __name__ == '__main__':
@@ -49,5 +76,6 @@ if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
 
     w = ClientUI()
+    w.show()
 
     sys.exit(app.exec_())
