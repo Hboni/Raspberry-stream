3ceac92d3082ef1d99607b759399f1f8c2e53945
Hugo BONIFACE <hb255304@is234212.intra.cea.fr>
2018-07-11 08:46:11 +0200

    Create a command line possibility to launch server

---
+++ rasp_server.py
@@ -0,0 +1,35 @@
+import os
+import argparse
+import socket
+
+### Mandatory parameters
+parser = argparse.ArgumentParser(description="Launch a raspberry stream server.")
+
+parser.add_argument('--address', '-ad', required=True,
+                    help='Address of the server')
+parser.add_argument('--port', '-p', required=True,
+                    help='Port to listen')
+
+### Optionnal parameters
+parser.add_argument('--timeout', '-t', required=False,
+                    help='Timeout of the server')
+
+## Get parameters in variables
+args = vars(parser.parse_args())
+address = args.pop('address')
+port = args.pop('port')
+timeout = args.pop('timeout')
+
+
+### Init of socket
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    socket.bind((address, port))
+    print('Server bind to {}:{}'.format((address, port)))
+except:
+    print('Server not able to bind to {}:{}'.format((address, port)))
+
+if timeout:
+    socket.settimeout(timeout)
+else:
+    socket.settimeout(2)
