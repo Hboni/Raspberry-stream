4db59efd17506ed424eb139e5fa825d52bf8c3df
Hugo <hugoboniface@gmail.com>
2018-07-12 23:41:13 +0200

    Use of server class in command executable

--- rasp_server.py
+++ rasp_server.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import socket
+from server import Server
 
 ### Mandatory parameters
 parser = argparse.ArgumentParser(description="Launch a raspberry stream server.")
@@ -20,16 +21,17 @@ address = args.pop('address')
 port = args.pop('port')
 timeout = args.pop('timeout')
 
-
 ### Init of socket
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    socket.bind((address, port))
-    print('Server bind to {}:{}'.format(address, port))
-except:
-    print('Server not able to bind to {}:{}'.format(address, port))
-
-if timeout:
-    socket.settimeout(timeout)
-else:
-    socket.settimeout(2)
+server = Server(address, port, timeout)
+server.begin_listen()
+# socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# try:
+#     socket.bind((address, port))
+#     print('Server bind to {}:{}'.format(address, port))
+# except:
+#     print('Server not able to bind to {}:{}'.format(address, port))
+#
+# if timeout:
+#     socket.settimeout(timeout)
+# else:
+#     socket.settimeout(2)
